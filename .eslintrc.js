8a9c7792d7dbea7e2dd6186cbffdec08bf4e7753
didiaohu <301085344@qq.com>
2018-09-19 01:56:46 +0800

    Revert ":tada:：初次提交代码，新增css基础样式，修改eslint规则，添加测试用例文件夹"
    This reverts commit bb76f9f4c35df56bae983f314bee625769ca26b9.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'linebreak-style': 'off',
-    'semi': ["error", "always"],
-    'space-before-function-paren':'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import './base/themes.scss';
-
-import module from './module';
-
-Vue.use(module);
-
-
 Vue.config.productionTip = false;
 
 new Vue({
