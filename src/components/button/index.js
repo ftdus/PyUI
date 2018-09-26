475a24c69f03e2fb71b08808925e72bb1057f65d
MountainC44 <1071011167@qq.com>
2018-09-26 20:20:19 +0800

    :sparkles:增加card组件
    增加card组件

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,7 +1,7 @@
 import PYButton from './src/button.vue';
 
 PYButton.install = function PyButton(Vue) {
-  Vue.component(PYButton.name, PYButton);
+    Vue.component(PYButton.name, PYButton);
 };
 
 export default PYButton;
