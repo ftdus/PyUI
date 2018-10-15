83ce03c5b74f0bf822847f71ab67bf22e5248f35
刘裕强 <414@op110.com>
2018-10-15 21:16:08 +0800

    新增upload文件上传组件

---
+++ src/testsDemo/upload/upload.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <py-upload
+          :format='format'
+          @fileSelect='fileSelect'>
+          <py-button>上传文件</py-button>
+        </py-upload>
+    </div>
+</template>
+
+<script>
+import PyUpload from '@/components/upload';
+import PYButton from '../../components/button';
+
+export default {
+  name: 'upload',
+  components: {
+    PyUpload, PYButton,
+  },
+  data () {
+    return {
+      format: ['jpg', 'png', 'txt']
+    }
+  },
+  methods: {
+    // 刚选择上传的文件
+    fileSelect (file, fileList) {
+      console.log(file, fileList)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
