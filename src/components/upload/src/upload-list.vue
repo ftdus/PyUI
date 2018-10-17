ac15ff45d11314c0459b440d9dbb347612437b49
ZongDuCha <1367243169@qq.com>
2018-10-18 00:31:22 +0800

    upload-list

---
+++ src/components/upload/src/upload-list.vue
@@ -0,0 +1,76 @@
+<template>
+  <ul class="file-list" v-if='files.length > 0'>
+    <li v-for='(item, index) in files' :key='index'>
+      <span class="name">{{item.name}}</span>
+      <div class="upload-wrap" >
+        <div class="upload-precc">
+          <li :style='{width: item.percentage+"%"}'></li>
+        </div>
+        <span class="del" ref='del'>{{item.percentage}}%</span>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "upload-list",
+  props: {
+    files: {
+      type: Array,
+      default: () => {}
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+@import "@/base/themes.scss";
+.file-list{
+  font-size: 15px;
+  padding-top: 7px;
+  padding-bottom: 7px;
+  display: flex;
+  flex-wrap:wrap;
+  margin-left: -5px;
+  li{
+    width: 100%;
+    border-radius: 3px;
+    transition: .3s;
+    padding:6px;
+    &:hover{
+      background: rgba(123, 180, 240, 0.1);
+    }
+    .upload-wrap{
+      height: 20px;
+      line-height: 20px;
+      display: flex;
+      .upload-precc{
+        flex: 1;
+        height: 2px;
+        background: #ebebeb;
+        margin-top: 8px;
+        li{
+          width: 0%;
+          background: linear-gradient(to right, $border-color-hover, $success-color);
+          height: 100%;
+          padding:0;
+          z-index: 2;
+          transition: 1s;
+        }
+      }
+    }
+  }
+  .name{
+    width:100%;
+    color:$color;
+    font-size: 14px;
+  }
+  .del{
+    width:45px;
+    color:$color;
+    font-size: 12px;
+    text-align: center;
+  }
+}
+</style>
