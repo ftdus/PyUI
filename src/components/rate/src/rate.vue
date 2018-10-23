37faaeb05112efa0923f0a7db52e8df5ae5a1acc
wangchloe <wangchloe@yeah.net>
2018-10-23 17:43:27 +0800

    :art:rate结构整理

--- src/components/rate/src/rate.vue
+++ src/components/rate/src/rate.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class="py-rate" @mouseleave="handleMouseleave">
+  <div :class="wrapClass" @mouseleave="handleMouseleave">
       <input type="hidden" :name="name" :value="currentValue">
       <div
-        v-for="item in count"
         class="iconfont"
         :class="handleCls(item)"
         @mousemove="handleMousemove(item, $event)"
-        :key="item"
-        @click="handleClick(item)">
+        @click="handleClick(item)"
+        v-for="item in count"
+        :key="item">
           <template v-if="!showCharacter">
-            <span :class="[prefixCls + '--content']" type="half"></span>
+            <span :class="[prefixCls + '__star--content']" type="half"></span>
           </template>
           <template v-else>
-            <span :class="[prefixCls + '-star-first']" type="half">
-              <template v-if="character !== ''">
-                {{character}}
-              </template>
-              <i v-else :class="iconClasses" type="half"></i>
+            <span :class="[prefixCls + '__char--half']" type="half">
+              <template v-if="character !== ''">{{character}}</template>
+              <span v-else :class="iconClasses" type="half"></span>
             </span>
-            <span :class="[prefixCls + '-star-second']">
-              <template v-if="character !== ''">
-                {{ character }}
-              </template>
-              <i v-else :class="iconClasses"></i>
+            <span :class="[prefixCls + '__char--content']">
+              <template v-if="character !== ''">{{ character }}</template>
+              <span v-else :class="iconClasses"></span>
             </span>
           </template>
       </div>
@@ -37,72 +33,72 @@
 </template>
 
 <script>
-const prefixCls = "py-rate__star";
+const prefixCls = "py-rate";
 
 export default {
   name: 'py-rate',
   props: {
     count: {
-        type: Number,
-        default: 5
+      type: Number,
+      default: 5
     },
     value: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     allowHalf: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     disabled: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     showText: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     name: {
-        type: String
+      type: String
     },
     character: {
-        type: String,
-        default: ''
+      type: String,
+      default: ''
     },
     icon: {
-        type: String,
-        default: ''
+      type: String,
+      default: ''
     },
     customIcon: {
-        type: String,
-        default: ''
+      type: String,
+      default: ''
     }
   },
   data() {
     return {
       prefixCls: prefixCls,
-      hoverInedx: -1,
+      hoverIndex: -1,
       isHover: false,
       isHalf: this.allowHalf && this.value.toString().indexOf(".") >= 0,
       currentValue: this.value,
     };
   },
   computed: {
-    classes () {
+    wrapClass () {
       return [
-          `${prefixCls}`,
-          {
-              [`${prefixCls}-disabled`]: this.disabled
-          }
+        `${prefixCls}`,
+        {
+            [`${prefixCls}-disabled`]: this.disabled
+        }
       ];
     },
     iconClasses () {
       return [
-          'py-icon',
-          {
-              [`py-icon-${this.icon}`]: this.icon !== '',
-              [`${this.customIcon}`]: this.customIcon !== '',
-          }
+        'py-icon',
+        {
+            [`py-${this.icon}`]: this.icon !== '',
+            [`${this.customIcon}`]: this.customIcon !== '',
+        }
       ];
     },
     showCharacter () {
@@ -111,73 +107,69 @@ export default {
   },
   watch: {
     value (val) {
-        this.currentValue = val;
+      this.currentValue = val;
     },
     currentValue (val) {
-        this.setHalf(val);
+      this.setHalf(val);
     }
   },
   methods: {
     handleCls (value) {
-        const hoverIndex = this.hoverIndex;
-        const currentIndex = this.isHover ? hoverIndex : this.currentValue;
+      const hoverIndex = this.hoverIndex;
+      const currentIndex = this.isHover ? hoverIndex : this.currentValue;
 
-        let full = false;
-        let isLast = false;
+      let full = false;
+      let isLast = false;
 
-        if (currentIndex >= value) full = true;
+      if (currentIndex >= value) full = true;
 
-        if (this.isHover) {
-            isLast = currentIndex === value;
-        } else {
-            isLast = Math.ceil(this.currentValue) === value;
-        }
+      if (this.isHover) {
+          isLast = currentIndex === value;
+      } else {
+          isLast = Math.ceil(this.currentValue) === value;
+      }
 
-        return [
-            {   
-                [`${prefixCls}`]: !this.showCharacter,
-                [`${prefixCls}--chart`]: this.showCharacter,
-                [`${prefixCls}--full`]: (!isLast && full) || (isLast && !this.isHalf),
-                [`${prefixCls}--half`]: isLast && this.isHalf,
-                [`${prefixCls}--zero`]: !full
-            }
-        ];
+      return [
+          {   
+              [`${prefixCls}__star`]: !this.showCharacter,
+              [`${prefixCls}__star--chart`]: this.showCharacter,
+              [`${prefixCls}__star--full`]: (!isLast && full) || (isLast && !this.isHalf),
+              [`${prefixCls}__star--half`]: isLast && this.isHalf,
+              [`${prefixCls}__star--zero`]: !full
+          }
+      ];
     },
     handleMousemove(value, event) {
-        if (this.disabled) return;
+      if (this.disabled) return;
 
-        this.isHover = true;
-        if (this.allowHalf) {
-            const type = event.target.getAttribute('type') || false;
-            this.isHalf = type === 'half';
-        } else {
-            this.isHalf = false;
-        }
-        this.hoverIndex = value;
+      this.isHover = true;
+      if (this.allowHalf) {
+          const type = event.target.getAttribute('type') || false;
+          this.isHalf = type === 'half';
+      } else {
+          this.isHalf = false;
+      }
+      this.hoverIndex = value;
     },
     handleMouseleave () {
-        if (this.disabled) return;
+      if (this.disabled) return;
 
-        this.isHover = false;
-        this.setHalf(this.currentValue);
-        this.hoverIndex = -1;
+      this.isHover = false;
+      this.setHalf(this.currentValue);
+      this.hoverIndex = -1;
     },
     setHalf (val) {
-        this.isHalf = this.allowHalf && val.toString().indexOf('.') >= 0;
+      this.isHalf = this.allowHalf && val.toString().indexOf('.') >= 0;
     },
     handleClick (value) {
-        if (this.disabled) return;
-        //value++;
-        if (this.isHalf) value -= 0.5;
+      if (this.disabled) return;
+      if (this.isHalf) value -= 0.5;
 
-        if(this.clearable && Math.abs(value - this.currentValue) < 0.01) {
-            value = 0;
-        }
+      if(Math.abs(value - this.currentValue) < 0.01) {
+          value = 0;
+      }
 
-        this.currentValue = value;
-        // this.$emit('input', value);
-        // this.$emit('on-change', value);
-        // this.dispatch('FormItem', 'on-form-change', value);
+      this.currentValue = value;
     }
   }
 };
@@ -186,44 +178,159 @@ export default {
 <style lang="scss" scoped>
 @import "@/base/themes.scss";
 @font-face {font-family: "iconfont";
-  src: url('iconfont.ttf?t=1539678122604') format('truetype'),
-  url('iconfont.svg?t=1539678122604#iconfont') format('svg');
+  src: url('iconfont.ttf?t=1540283022116') format('truetype'),
+  url('iconfont.svg?t=1540283022116#iconfont') format('svg');
 }
 
 .iconfont {
   font-family:"iconfont" !important;
-  font-size:16px;
+  font-size:20px;
   font-style:normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
-.py-rate__star:before { content: "\e82b"; color: #ccc; }
-.py-rate__star--half:before, .py-rate__star--full:before { color: #f5a623; }
+.py-icon-heart:before { content: "\e610"; }
+.py-icon-livestar:before { content: "\e711"; }
+.py-icon-liveheart:before { content: "\e6ec"; }
 
-.py-rate {
-  color: $color;
-  * {
-    box-sizing: border-box;
+$ratePrefix: "py-rate";
+$font: "\e6f9";
+$gray: #e9e9e9;
+$orange: #f5a623;
+$yellow: #f7b84f;
+
+.#{$ratePrefix} {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+  vertical-align: middle;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.#{$ratePrefix}-disabled {
+  .#{$ratePrefix}__star--content:before {
+    cursor: default;
   }
-  .py-rate__star {
-    display: inline-block;
-    position: relative;
-    margin-right: 8px;
-  }
-  .py-rate__star__content { 
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 50%;
-    height: 100%;
-    overflow: hidden;
+  .#{$ratePrefix}__star {
     &:before {
-      color: #ccc;
+      cursor: default;
+    }
+    &:hover {
+      -webkit-transform: scale(1);
+      -ms-transform: scale(1);
+      transform: scale(1);
     }
   }
-  .py-rate__star__ext { 
-    display: inline-block;
+}
+
+.#{$ratePrefix}__star--full, .#{$ratePrefix}__star--zero {
+  position: relative;
+}
+
+.#{$ratePrefix}__char--half {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  opacity: 0;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-transition: all .3s ease;
+  transition: all .3s ease;
+  color: $gray;
+  cursor: pointer;
+}
+
+.#{$ratePrefix}__char--content {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-transition: all .3s ease;
+  transition: all .3s ease;
+  color: $gray;
+  cursor: pointer;
+}
+
+.#{$ratePrefix}__star--chart {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  margin-right: 8px;
+  position: relative;
+  -webkit-transition: all .3s ease;
+  transition: all 0.3s ease;
+  &:hover {
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
   }
+  &.#{$ratePrefix}__star--full {
+    .#{$ratePrefix}__char--half, .#{$ratePrefix}__char--content {
+      color: $orange;
+    }
+  }
+  &.#{$ratePrefix}__star--half .#{$ratePrefix}__char--half {
+    opacity: 1;
+    color: $orange;
+  }
+}
+
+.#{$ratePrefix}__star {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  margin-right: 8px;
+  position: relative;
+  -webkit-transition: all .3s ease;
+  transition: all 0.3s ease;
+  &:hover {
+    -webkit-transform: scale(1.1);
+    -ms-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+}
+
+.#{$ratePrefix}__star--content:before, .#{$ratePrefix}__star:before {
+  color: $gray;
+  cursor: pointer;
+  content: $font;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  display: block;
+}
+
+.#{$ratePrefix}__star--content {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+  &:before {
+    color: transparent;
+  }
+}
+
+.#{$ratePrefix}__star--full:before, .#{$ratePrefix}__star--half .#{$ratePrefix}__star--content:before {
+  color: $orange;
+}
+
+.#{$ratePrefix}__star--full:hover:before, .#{$ratePrefix}__star--half:hover .#{$ratePrefix}__star--content:before {
+  color: $yellow;
+}
+
+.#{$ratePrefix}__text {
+  margin-left: 8px;
+  vertical-align: middle;
+  display: inline-block;
+  font-size: 12px;
 }
 </style>
--- src/testsDemo/rate/rate.vue
+++ src/testsDemo/rate/rate.vue
@@ -1,28 +1,50 @@
 <template>
   <div class="page">
-    <h2>基础用法</h2>
+    <h2>基础用法：`&lt;py-rate&gt;&lt;/py-rate&gt;`</h2>
     <section>
       <py-rate v-model="value"></py-rate>
     </section>
 
-    <h2>半星</h2>
+    <h2>满分指数：`&lt;py-rate :count="7" &gt;&lt;/py-rate&gt;`</h2>
     <section>
-      <py-rate v-model="valueHalf"></py-rate>
+      <py-rate :count="7" v-model="value"></py-rate>
     </section>
 
-    <h2>显示提示</h2>
+    <h2>半星：`&lt;py-rate allow-half&gt;&lt;/py-rate&gt;`</h2>
     <section>
-      <py-rate show-text v-model="valueHalf"></py-rate>
+      <py-rate allow-half v-model="valueHalf"></py-rate>
     </section>
 
-    <h2>只读</h2>
+    <h2>显示提示：`&lt;py-rate show-text&gt;&lt;/py-rate&gt;`</h2>
     <section>
-      <py-rate disabled v-model="valueHalf"></py-rate>
+      <py-rate show-text v-model="valueText"></py-rate>
     </section>
 
-    <h2>自定义字符或图标</h2>
+    <h2>只读：`&lt;py-rate disabled&gt;&lt;/py-rate&gt;`</h2>
+    <section>
+      <py-rate disabled v-model="valueText"></py-rate>
+    </section>
+
+    <h2>自定义字符：`&lt;py-rate character="A"&gt;&lt;/py-rate&gt;`</h2>
+    <section>
+      <py-rate allow-half character="A" v-model="valueHalf"></py-rate>
+    </section>
     <section>
       <py-rate allow-half character="好" v-model="valueHalf"></py-rate>
+    </section>
+
+    <h2>自定义图标：`&lt;py-rate icon="icon-heart"&gt;&lt;/py-rate&gt;`</h2>
+    <section>
+      <py-rate allow-half icon="icon-heart" v-model="valueHalf"></py-rate>
+    </section>
+    <section>
+      <py-rate allow-half icon="icon-livestar" v-model="valueHalf"></py-rate>
+    </section>
+    <section>
+      <py-rate allow-half icon="icon-liveheart" v-model="valueHalf"></py-rate>
+    </section>
+    <section>
+      <py-rate allow-half icon="icon-liveheart" v-model="valueHalf"></py-rate>
     </section>
   </div>
 </template>
