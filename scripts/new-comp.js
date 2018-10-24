690643fa910886d542e1f76f99f586ce025889ee
DaiChangchun <shiying.dcc@raycloud.com>
2018-10-24 20:25:17 +0800

    :construction: 组件新建脚本更新

--- scripts/new-comp.js
+++ scripts/new-comp.js
@@ -7,6 +7,16 @@ const inquirer = require('inquirer');
 const { readText, nameFormat } = require('./utils/tools');
 const promps = require('./config/promps');
 
+const templates = path.resolve(__dirname, `./templates/components`);
+const srcPath = path.resolve(__dirname, `../src`);
+// 源文件map
+const sourceFiles = {
+  index: path.resolve(templates, `index.js`),
+  vue: path.resolve(templates, `name.vue`),
+  scss: path.resolve(templates, `name.scss`),
+};
+
+let spinner;
 /**
  * 快麦ERP-小程序: 快捷模板构建
  */
@@ -14,7 +24,13 @@ const promps = require('./config/promps');
 async function doTemplate() {
   // 模板类型,模板名称
   let { name } = await inquirer.prompt(promps.comptName);
-  createTemplate(name);
+  try {
+    spinner = ora('开始创建组件模板').start();
+    createTemplate(name);
+  } catch (error) {
+    spinner.fail(`组件 ${name} 的目录结构创建失败,错误信息如下:`);
+    console.error(error);
+  }
 }
 
 /**
@@ -23,61 +39,47 @@ async function doTemplate() {
  * @param {String} type 模板类型 pages or components
  */
 function createTemplate(name) {
-  const spinner = ora('开始创建组件模板').start();
+  const compPath = path.resolve(__dirname, `../src/components/${name}`);
+  // 目标文件map
+  const distFiles = {
+    index: path.resolve(compPath, `index.js`),
+    vue: path.resolve(compPath, `src/${name}.vue`),
+    scss: path.resolve(srcPath, `styles/src/${name}.scss`),
+  };
 
-  const router = path.resolve(__dirname, `../src/components/${name}`);
-  const sourceIndex = path.resolve(
-    __dirname,
-    `./templates/components/index.js`
-  );
-  const sourceVue = path.resolve(
-    __dirname,
-    `./templates/components/src/name.vue`
-  );
-  const sourceScss = path.resolve(
-    __dirname,
-    `./templates/components/name.scss`
-  );
-
-  const distIndex = path.resolve(
-    __dirname,
-    `../src/components/${name}/index.js`
-  );
-  const distVue = path.resolve(
-    __dirname,
-    `../src/components/${name}/src/${name}.vue`
-  );
-  const distScss = path.resolve(__dirname, `../src/styles/src/${name}.scss`);
-
-  if (fs.existsSync(router)) {
+  if (fs.existsSync(compPath)) {
     spinner.fail(`组件 ${name} 已经存在,无法创建`);
   } else {
-    let idxContent = readText(sourceIndex);
-    idxContent = idxContent.replace('{{__component_name__}}', name);
-    idxContent = idxContent.replace(
-      /\{\{__py_component_name__\}\}/g,
-      `Py` + nameFormat(name)
-    );
-
     // 输出index.js
-    fs.createFileSync(distIndex);
-    fs.writeFileSync(distIndex, idxContent);
+    let idxContent = readText(sourceFiles.index);
+    idxContent = idxContent
+      .replace('{{__component_name__}}', name)
+      .replace(/\{\{__py_component_name__\}\}/g, `Py` + nameFormat(name));
+    fs.createFileSync(distFiles.index);
+    fs.writeFileSync(distFiles.index, idxContent);
 
     // 输出默认的vue文件
-    fs.createFileSync(distVue);
-    fs.writeFileSync(
-      distVue,
-      readText(sourceVue).replace(
-        /\{\{__py_component_name__\}\}/g,
-        `Py` + nameFormat(name)
-      )
-    );
+    fs.createFileSync(distFiles.vue);
+    fs.writeFileSync(distFiles.vue, replaceCompName('vue', name));
 
     // 输出默认的scss文件
-    fs.copyFileSync(sourceScss, distScss);
+    fs.createFileSync(distFiles.scss);
+    fs.writeFileSync(distFiles.scss, replaceCompName('scss', name));
 
     spinner.succeed(`组件 ${name} 的目录结构已完成初始化`);
   }
+}
+
+/**
+ * 替换模板内占位符为模板名称
+ * @param {String} type 文件类型
+ * @param {String} name 模板mingc
+ */
+function replaceCompName(type, name) {
+  return readText(sourceFiles[type]).replace(
+    /\{\{__py_component_name__\}\}/g,
+    `py-${name}`
+  );
 }
 
 doTemplate();
--- scripts/templates/components/name.vue
+++ scripts/templates/components/name.vue
@@ -1,8 +1,11 @@
 <template></template>
 
 <script>
+// 组件名 / class前缀
+const prefixCls = '{{__py_component_name__}}';
+
 export default {
-  name: '{{__py_component_name__}}',
+  name: prefixCls,
 
   inject: {},
 
