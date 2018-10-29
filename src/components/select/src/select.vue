a94b45ff2de1d728b383b989163c27ce5efda6f4
Rachel <rachelx@myuniuni.com>
2018-10-29 09:01:44 +0800

    :tada: 初次提交代码,初始化select组件

---
+++ src/components/select/src/select.vue
@@ -0,0 +1,20 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+// 组件class前缀
+const prefixCls = 'py-select';
+console.log(prefixCls);
+export default {
+  name: 'PySelect',
+
+  inject: {},
+
+  props: {},
+
+  computed: {},
+
+  methods: {},
+};
+</script>
