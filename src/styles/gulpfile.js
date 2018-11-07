d8064a6b9134ba63959101d1fae022fdd14f65ee
YuLinXi <15708444077@163.com>
2018-11-07 18:03:31 +0800

    优化CSS构建

--- src/styles/gulpfile.js
+++ src/styles/gulpfile.js
@@ -2,16 +2,24 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-cssmin');
+const gulpHeader = require('gulp-header');
 
-gulp.task('compile', () => gulp
-  .src('./src/*.scss')
-  .pipe(sass.sync())
-  .pipe(autoprefixer({
-    browsers: ['ie > 9', 'last 2 versions'],
-    cascade: false,
-  }))
-  .pipe(cssmin())
-  .pipe(gulp.dest('./lib')));
+gulp.task('compile', () =>
+  gulp
+    .src('./src/*.scss')
+    .pipe(gulpHeader(`
+    @import './base/mixins/config';\n
+    @import './base/common/themes';\n
+    @import './base/mixins/utils';\n
+    @import './base/mixins/mixins';\n
+  `))
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      browsers: ['ie > 9', 'last 2 versions'],
+      cascade: false,
+    }))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./lib')));
 
 // gulp.task('copyfont', function() {
 //   return gulp
